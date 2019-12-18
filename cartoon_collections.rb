33b548243aa09377970c28ff8c16f7c2488da31e
kowalix007 <kowalix007@gmail.com>
2019-12-18 15:44:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,6 @@ end
 def find_the_cheese(cheese)  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.include?("cheddar") 
-else
-  nil 
+  potentially_cheesy_item.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
 end
