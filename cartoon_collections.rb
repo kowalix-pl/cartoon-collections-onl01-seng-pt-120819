a5e7bb57511e9a41a91065d9a1175729f5063514
kowalix007 <kowalix007@gmail.com>
2019-12-18 15:38:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,10 +9,10 @@ end
 
 def long_planeteer_calls (planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
-
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types[0]
 end
